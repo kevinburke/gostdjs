eef3bc24fd2a9635d415a6b41fe9261580296ca3
Kevin Burke <kev@inburke.com>
2017-09-12 10:47:27 +0200

    add LICENSE and preamble to all files

--- bytes/index.js
+++ bytes/index.js
@@ -1,3 +1,6 @@
+// gostdjs version 0.1
+// Copyright 2017 The Go Authors. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
 "use strict";
 
 const internal = require('../internal/index.js');
---
+++ go_compat/README.md
@@ -0,0 +1,3 @@
+This package contains compatibility tests with the Go standard library, and some
+experiments with e.g. bytes and sizes. You shouldn't want or need to import this
+package into your Javascript program.
